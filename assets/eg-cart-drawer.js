e3dee1ca42dc72308f87105c1d0284f75fc66cb7
Evan Glover <evan.m.glover@gmail.com>
2023-11-01 14:07:38 -0400

    cart drawer upsell atc fix

--- assets/eg-cart-drawer.js
+++ assets/eg-cart-drawer.js
@@ -196,8 +196,8 @@ class EGCartDrawer extends HTMLElement {
         })
         .then((state) => {
           const parsedState = JSON.parse(state);
-          // update new information to the cart by triggering the async refresh function in the quantity controller of another line item.
-          this.closest("eg-cart-drawer").querySelector("eg-cart-drawer-qty").renderCartDrawerUpdates();
+          // update new information to the cart by triggering the async refresh function of the cart drawer
+          this.closest("eg-cart-drawer").renderCartDrawerUpdates();
         });
     }
   }
